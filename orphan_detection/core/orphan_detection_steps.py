5298d188dc080284dbeb9b4374b7934cba093f54
felix.etzkorn@hm.edu <felix.etzkorn@web.de>
2022-06-17 11:32:47 +0200

    fixed pylint linting errors

--- orphan_detection/core/orphan_detection_steps.py
+++ orphan_detection/core/orphan_detection_steps.py
@@ -97,7 +97,7 @@ def check_status_codes(domain: str, probe_args: util.ProbeParameters) -> int:
     probe_candidates_path = constants.CANDIDATES_TO_PROBE_LIST_NAME_TEMPLATE.format(DOMAIN=domain)
     probe_candidates = util.read_lines_from_file(probe_candidates_path)
 
-    potential_orphans = {}
+    potential_orphans = []
     all_status_codes = {}
     error_responses = {}
 
@@ -108,20 +108,19 @@ def check_status_codes(domain: str, probe_args: util.ProbeParameters) -> int:
         status_code, error_msg = util.probe_url(url, timeout_after=probe_args.timeout)
 
         if status_code == 200:
-            potential_orphans[url] = url  # TODO SET?
+            potential_orphans.append(url)
 
         if error_msg is not None:
             error_responses[url] = f"{error_msg:25s} {url}"
 
         all_status_codes[url] = f"{status_code:03} {url}"
-        time.sleep(probe_args.interval)
 
-    potential_orphans_sorted = [potential_orphans[url] for url in probe_candidates if url in potential_orphans]
+    potential_orphans.sort()
     all_status_codes_sorted = [all_status_codes[url] for url in probe_candidates]
     error_responses_sorted = [error_responses[url] for url in probe_candidates if url in error_responses]
 
     potential_orphans_path = constants.POTENTIAL_ORPHAN_LIST_NAME_TEMPLATE.format(DOMAIN=domain)
-    util.write_lines_to_file(potential_orphans_path, potential_orphans_sorted)
+    util.write_lines_to_file(potential_orphans_path, potential_orphans)
 
     status_code_list_path = constants.STATUS_CODES_LIST_NAME_TEMPLATE.format(DOMAIN=domain)
     util.write_lines_to_file(status_code_list_path, all_status_codes_sorted)
@@ -129,4 +128,4 @@ def check_status_codes(domain: str, probe_args: util.ProbeParameters) -> int:
     error_responses_path = constants.ERROR_RESPONSES_LIST_NAME_TEMPLATE.format(DOMAIN=domain)
     util.write_lines_to_file(error_responses_path, error_responses_sorted)
 
-    return len(potential_orphans_sorted)
+    return len(potential_orphans)
